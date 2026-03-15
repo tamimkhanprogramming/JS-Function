b619b6aab2089a6a140d90edcedf53d16b9f718c
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-15 23:55:52 +0600

    Updated

---
+++ arguments.js
@@ -0,0 +1,12 @@
+function doubleIt (number){
+
+    const doubled = number * 2;
+    console.log('The Number I Given To Multiply is: ',number,doubled);
+}
+
+console.log('I Will call the function');
+doubleIt(15);
+console.log('-------');
+doubleIt(88);
+console.log('-------');
+doubleIt(873);
