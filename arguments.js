d31753f128da977173fca6019cc8a144ef1d8e0e
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-16 01:08:54 +0600

    Updated

--- arguments.js
+++ arguments.js
@@ -10,3 +10,22 @@ console.log('-------');
 doubleIt(88);
 console.log('-------');
 doubleIt(873);
+
+const number = 55;
+doubleIt(number);
+
+const money = 112;
+doubleIt(money);
+
+function difference(num1,num2){
+    const diff_num = num1 - num2;
+
+    console.log(num1,num2,'Difference',diff_num);
+}
+
+const father_age = 63;
+
+const son_age = 27;
+
+difference(father_age,son_age);
+
