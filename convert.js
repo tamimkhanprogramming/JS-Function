1feb7ddb419370d3726e76c4bcad969483014307
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-24 20:05:10 +0600

    Updated

--- convert.js
+++ convert.js
@@ -1,12 +1,37 @@
-//  Calculate Height //
+// //  Calculate Height //
 
 function inchToFeet(inch){
 
-    const height = inch/12;
+    const feet  = inch / 12;
 
-    return height;
+    return feet;
 }
 
-const myHeight = inchToFeet(75);
+const myHeight = inchToFeet(71);
 
-console.log(myHeight);
+console.log("Height is: ",myHeight);
+
+// Calculate Miles To KM
+
+function milesToKM(mile){
+
+    const mileTo_km = mile * 1.609344;
+    return mileTo_km;
+}
+
+const Calculate_Mile_To_KM = milesToKM(10);
+
+console.log("Miles Are: ",Calculate_Mile_To_KM);
+
+// Kilometer To Miles Formula
+
+ function kmToMiles(Kilometer){
+
+    const Kilometer_To_Miles = Kilometer * 0.621371;
+
+    return Kilometer_To_Miles;
+ }
+
+ const KilometerToMIles = kmToMiles(1);
+
+ console.log("Kilometer Are: ",KilometerToMIles);
